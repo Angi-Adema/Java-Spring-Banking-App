67c0c0f2071508f8650153390377623b69f03acf
Angi-Adema <dr.adema@gmail.com>
2025-06-21 00:09:21 -0600

    initial setup files

---
+++ furtureforwarddemo/src/main/java/com/futureforward/furtureforwarddemo/FurtureforwarddemoApplication.java
@@ -0,0 +1,13 @@
+package com.futureforward.furtureforwarddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurtureforwarddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurtureforwarddemoApplication.class, args);
+	}
+
+}
---
+++ furtureforwarddemo/src/test/java/com/futureforward/furtureforwarddemo/FurtureforwarddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futureforward.furtureforwarddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurtureforwarddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
