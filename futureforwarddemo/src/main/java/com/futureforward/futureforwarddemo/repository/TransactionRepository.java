91a52b0a938c55d3a30cdd639e7ae32d0244fb6b
Angi-Adema <dr.adema@gmail.com>
2025-08-01 13:36:39 -0600

    removed the optional @Repository in TransactionRepository.java

--- futureforwarddemo/src/main/java/com/futureforward/futureforwarddemo/repository/TransactionRepository.java
+++ futureforwarddemo/src/main/java/com/futureforward/futureforwarddemo/repository/TransactionRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     
     List<Transaction> findBySourceAccountIdOrDestinationAccountId(Long sourceId, Long destinationId);
