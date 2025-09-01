6255404bb0f64d9f39edf7f6026491fb0c29a504
Angi-Adema <dr.adema@gmail.com>
2025-09-01 14:47:05 -0600

    updated the notes in CustomerResponse.java to explain functionality

--- src/main/java/com/futureforward/futureforwarddemo/dto/CustomerResponse.java
+++ src/main/java/com/futureforward/futureforwarddemo/dto/CustomerResponse.java
@@ -1,3 +1,5 @@
+// This DTO defines the shape of the JSON response that the API sends back to the client when they request customer data.
+// When it sends customer data to the outside world (Swagger, front-end, mobile app, payment service), this is all that will be shared.
 package com.futureforward.futureforwarddemo.dto;
 
 import lombok.Data;
