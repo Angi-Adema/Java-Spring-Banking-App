6becfab55b971514cc3c39beab7c624ec41f4481
Angi-Adema <dr.adema@gmail.com>
2025-09-01 18:40:08 -0600

    added notes to DepositRequest.java dto file documenting the usage of the getters/setters outlined here

--- src/main/java/com/futureforward/futureforwarddemo/dto/DepositRequest.java
+++ src/main/java/com/futureforward/futureforwarddemo/dto/DepositRequest.java
@@ -10,6 +10,8 @@ public class DepositRequest {
     @Positive(message = "amount must be > 0")
     private double amount;
 
+    // Getters/Setters set up here so other parts of the program such as services, controllers or serialization libraries such as Jackson
+    // can read the private set fields in a safe and controlled way. These fields are marked private so they cannot be accessed directly outside the class.
     public Long getAccountId() { return accountId; }
     public void setAccountId(Long accountId) { this.accountId = accountId; }
     public double getAmount() { return amount; }
